de5873e8ce6a816a9a4650f99d0648ff68a78b0f
AlexBDominguez <alexwirtzfp@gmail.com>
2025-05-26 09:10:27 +0200

    Ejercicio 14 Testeado

--- src/main/java/com/campusdual/classroom/Person.java
+++ src/main/java/com/campusdual/classroom/Person.java
@@ -4,8 +4,20 @@ public class Person {
 	protected String name;
 	protected String surname;
 
+
 	public Person(String name, String surname) {
 		this.name = name;
 		this.surname = surname;
+	}
+
+	public void getDetails(){
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("El nombre es ");
+		sb.append(name);
+		sb.append(" y su apellido ");
+		sb.append(surname);
+
+		System.out.println(sb.toString());
 	}
 }
